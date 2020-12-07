e6099ecc29963dcc8dffff5678fe2a17f5c6cdae
Vlad Sirenko <44810093+MrFlava@users.noreply.github.com>
2020-12-07 09:32:07 +0200

    Add files via upload

--- src/main/java/com/example/Human.java
+++ src/main/java/com/example/Human.java
@@ -1,46 +1,164 @@
-package com.example;
-
-public class Human {
-    private String  name;
-    private String  surname;
-    private String  patronymic;
+package lab3;
 
 
-    public enum Sex{
-        MAN,
-        WOMAN
-    };
 
-    private Sex sex;
 
-    public String getName(){
-        return name;
+
+import lab3.controllers.UniversityCreatorManually;
+
+import lab3.controllers.UniversityRandomCreator;
+
+import lab3.models.universityUnits.University;
+
+
+
+
+import java.util.Scanner;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        University university = createUniversity();
+
+        System.out.println(university);
+
     }
 
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getSurname(){
-        return surname;
+
+
+
+    public static University createUniversity() throws Exception {
+
+        University university;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        System.out.print(Constants.ANSI_PURPLE + "Do you want to create university manually? (y/n): ");
+
+        boolean isManually = "y".equals(scanner.nextLine());
+
+
+
+
+        if (isManually) {
+
+            university = new UniversityCreatorManually().run();
+
+        } else {
+
+            university = new UniversityRandomCreator().run();
+
+        }
+
+        return university;
+
     }
 
-    public void setSurname(String surname){
-        this.surname=surname;
-    }
-
-    public String getPatronymic(){
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic){
-        this.patronymic=patronymic;
-    }
-
-    public Sex getSex(){
-        return sex;
-    }
-
-    public void setSex(Sex sex){
-        this.sex=sex;
-    }
 }
+
+
+package lab3;
+
+
+
+
+
+import lab3.models.Sex;
+
+
+
+
+import java.util.List;
+
+
+
+
+public class Utils {
+
+
+
+
+    public static String listToString(List<?> list, int indent) {
+
+        String tab_indent = resolveIndent(indent);
+
+
+
+
+        StringBuilder result = new StringBuilder("");
+
+        for (Object o : list) {
+
+            result.append("\n").append(tab_indent).append(o);
+
+        }
+
+        return result.toString();
+
+    }
+
+
+
+
+    private static String resolveIndent(int indent) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < indent; i++) {
+
+            result.append("\t");
+
+        }
+
+        return result.toString();
+
+    }
+
+
+
+
+    public static Sex defineSex(String input) {
+
+        Sex sex;
+
+
+
+
+        switch(input) {
+
+            case "male":
+
+            case "m":
+
+                sex = Sex.MALE;
+
+                break;
+
+            case "female":
+
+            case "f":
+
+                sex = Sex.FEMALE;
+
+                break;
+
+            default:
+
+                throw new IllegalStateException("Unexpected value: " + input);
+
+        }
+
+        return sex;
+
+    }
+
+}
+
+
