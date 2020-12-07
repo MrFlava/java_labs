69c9f6b0b57efbba95a6533dcf3afbcf8da66878
Vlad Sirenko <44810093+MrFlava@users.noreply.github.com>
2020-12-07 09:23:28 +0200

    Update lab2.java

--- src/main/java/com/example/lab2.java
+++ src/main/java/com/example/lab2.java
@@ -1,127 +1,116 @@
-package com.example;
+package lab2;
+
+
+
+
+
+import lab2.matrix.Matrix;
+
+import lab2.matrix.MatrixCalculator;
+
+import lab2.matrix.MatrixManager;
+
+
+
 
 import java.util.Scanner;
-import java.lang.Math;
 
-public class lab2 {
 
-    private static int[][] randomFilling(int[][] array){
-        final int RANGE = 30;
-        final int DISTANCE = 15;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j]= (int) Math.round((Math.random()*RANGE)-DISTANCE);
-            }
-        }
-        return array;
-    }
-    private static int [][] manualFilling(int[][] array){
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(Lab1Constants.WELCOME_MESSAGE);
+
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print("[" + i + "]" + "[" + j + "] ");
-                array[i][j] = scanner.nextInt();
-            }
-        }
-        return array;
-    }
 
-    private static int [][] printArray(int[][] array){
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
-        return array;
-    }
-    private static int findMax(int[][] array){
 
-        int maxValue=array[0][0];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if(array[i][j]>maxValue){
-                    maxValue=array[i][j];
-                }
-            }
-        }
-        return maxValue;
-    }
-    private static int findMin(int[][] array){
 
-        int minValue=array[0][0];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if(array[i][j]<minValue){
-                    minValue=array[i][j];
-                }
-            }
-        }
-        return minValue;
-    }
 
-    private static double arithmeticAvenger(int[][] array){
-        float aveArray=0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                aveArray+=array[i][j];
-            }
-        }
-        aveArray=aveArray/(array.length*array[0].length);
+        // Read width
 
-        return aveArray;
-    }
-    private static double geometricAvenger(int [][] array){
-        double aveArray=1;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                aveArray*=array[i][j];
-            }
-        }
-        double power = (double) 1/(array.length*array[0].length);
-        aveArray=Math.pow(aveArray,power);
-        return aveArray;
-    }
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What input is needed, manual or random?(1/0): ");
-        int example = scanner.nextInt();
+        System.out.println("Enter matrix height below:");
 
-        System.out.print("Input length line array: ");
-        int line = scanner.nextInt();
+        int matrix_height = Integer.parseInt(scanner.nextLine());
 
-        System.out.print("Input length column array: ");
-        int column = scanner.nextInt();
 
-        if(line>20 ){
-            line = 20;
-            System.out.print("Will be 20");
-        }
 
-        if(column>20 ){
-            column = 20;
-            System.out.print("Will be 20");
-        }
 
-        int[][] array = new int[line][column];
+        // Read height
 
-        switch (example)      {
-            case 0:
-                array=randomFilling(array);
-                break;
-            case 1:
-                array=manualFilling(array);
-                break;
-            default:
-                System.out.println("Option don`t choose");
-        }
+        System.out.println("Enter matrix width below:");
 
-        printArray(array);
+        int matrix_width = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Max value: 0"+findMax(array));
-        System.out.println("Min value: "+findMin(array));
-        System.out.println("Arithmetic avenger value: "+arithmeticAvenger(array));
-        System.out.println("Geometric avenger value: "+geometricAvenger(array));
+
+
+
+        // Read is_random var, it means should we create matrix in random way
+
+        System.out.println("Write 'y' if you want to fill matrix in random way.");
+
+        boolean is_random = "y".equals(scanner.nextLine());
+
+
+
+
+        // matrix creation
+
+        MatrixManager matrixManager = new MatrixManager();
+
+        Matrix matrix = matrixManager.create_matrix(is_random, matrix_width, matrix_height);
+
+
+
+
+        matrixManager.validateMatrixParameters(matrix);
+
+        matrixManager.showMatrix(matrix);
+
+
+
+
+
+
+        // process calculations
+
+        MatrixCalculator matrixCalculator = new MatrixCalculator();
+
+
+
+
+        double average = matrixCalculator.calculateMatrixAverage(matrix);
+
+        int minimumElement = matrixCalculator.getMatrixMinimumElement(matrix);
+
+        int maximumElement = matrixCalculator.getMatrixMaximumElement(matrix);
+
+
+
+
+
+
+
+        System.out.printf("\nAverage value: %f\n", average);
+
+        System.out.printf("Minimum value: %d\n", minimumElement);
+
+        System.out.printf("Maximum value: %d\n", maximumElement);
 
     }
+
 }
+
+package lab2;
+
+
+
+public interface Lab1Constants {
+
+    int UPPER_RANDOM_RANGE = 100;
+
+    int LOWER_RANDOM_RANGE = 1;
